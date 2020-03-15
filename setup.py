34ac90a4ce9bc60ad0f404259b25a676c7bc92cc
Kukushkin Alexander <alex@alexkuk.ru>
2020-03-15 10:29:35 +0300

    More meta

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
 
 from setuptools import setup, find_packages
 
+
+with open('README.md') as file:
+    description = file.read()
+
+
 setup(
     name='corus',
     version='0.5.0',
+    description='Links to russian corpora, functions for loading and parsing',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/natasha/corus',
+    author='Alexander Kukushkin',
+    author_email='alex@alexkuk.ru',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    keywords='corpora, russian, nlp, datasets',
     install_requires=[],
     packages=find_packages(),
 )
+
